c6101408caa59974d45777d9197c77bd7cf704f8
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-20 11:28:49 -0700

    Add to numToRoman function in scripts.js to pass spec  #2
    Co-authored-by: Mark Tiapkov <mark.tiapkov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-// Behavior: When a user enters a [1, 5, 10, 50, 100, 500, 1,000], the function returns that correct roman number.
-// Input: "1, 5, 10, 50, 100, 500, 1,000"
-// Output: "I, V, X, L, C, D, M"
 
-// Behavior: When a user enters 2, 3, and 4, the function returns the correct roman numeral.
-// Input: "2, 3, 4"
-// Output: "II, III, IV"
 
 // Behavior: When a user enters 6-9 the function returns the correct roman numeral.
 // Input: "6 7 8 9"
@@ -25,10 +19,20 @@
 
 // Business Logic
 
+const romanArray = ["I", "V", "X", "L", "C", "D", "M", "O"]
+
 function numToRomanNum (num) {
   let result;
-  if (num === 1){
-    result = "I";
+  let romanArrayIndex;
+  let reLessThan5 = /[1-4]/;
+  if (reLessThan5.test(num)){
+    romanArrayIndex = 0;
+    if (num === "1" || num === "2" || num === "3"){
+      result = romanArray[romanArrayIndex].repeat(parseInt(num));
+    } else {
+    result = romanArray[romanArrayIndex].concat(romanArray[romanArrayIndex+1]);
+    };
+    
   } else if (num ===5){
     result =  "V";
   } else if (num === 10){
@@ -42,7 +46,6 @@ function numToRomanNum (num) {
   } else if (num === 1,000){
     result =  "M";
   } else {
-    // alert ("Plese enter another number.")
     result = "0";
   }
   return result;
@@ -51,8 +54,25 @@ function numToRomanNum (num) {
 
 // Test 1
 // When a user enters a [1, 5, 10, 50, 100, 500, 1,000], the function returns that correct roman number.
-expectedReturn = "I";
-console.log(numToRomanNum(1) === expectedReturn); // Expect true
+// expectedReturn = "I";
+// console.log(numToRomanNum(1) === expectedReturn); // Expect true
 
+// expectedReturn = "I";
+// console.log(numToRomanNum(2) === expectedReturn); // Expect false
+
+
+// // Test 2
+// When a user enters 2, 3, and 4, the function returns the correct roman numeral.
 expectedReturn = "I";
-console.log(numToRomanNum(2) === expectedReturn); // Expect false
+console.log("Test with 1 : " + (numToRomanNum("1") === expectedReturn)); // Expect true
+
+expectedReturn = "II";
+console.log("Test with 2 : " + (numToRomanNum("2") === expectedReturn)); // Expect true
+
+expectedReturn = "III";
+console.log("Test with 3 : " + (numToRomanNum("3") === expectedReturn)); // Expect true
+
+expectedReturn = "IV";
+console.log("Test with 4 : " + (numToRomanNum("4") === expectedReturn)); // Expect true
+
+
